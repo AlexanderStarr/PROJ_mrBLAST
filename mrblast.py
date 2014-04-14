76951dacb592474f14ce9d9ed76ca4bf832fef6e
Alexander Starr <AlexanderLStarr@gmail.com>
2014-04-14 18:21:08 -0500

    Have the script now accepting the query string from a file.

--- mrblast.py
+++ mrblast.py
@@ -3,19 +3,25 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-QUERY = "AACCCTAACCCT"
-
 class mrBLAST(MRJob):
+
+    def mapper_init(self):
+        # Provide the query, located in a file, to the mapper.
+        qfile = open('query.txt', 'r')
+        self.query = qfile.read().strip()
+        qfile.close()
+
     def mapper_get_lines(self, _, line):
-        if QUERY in line:
-            yield (QUERY, line)
+        if self.query in line:
+            yield (self.query, line)
 
     def reducer_show_occurrences(self, key, values):
         yield (key, str(list(values)))
 
     def steps(self):
-        return [MRStep(mapper=self.mapper_get_lines,
-                reducer=self.reducer_show_occurrences)]
+        return [MRStep(mapper_init=self.mapper_init,
+                       mapper=self.mapper_get_lines,
+                       reducer=self.reducer_show_occurrences)]
 
 if __name__ == '__main__':
     mrBLAST.run()
